dc7118e01cb3766762c7458394f3130d56bf5a57
Nsuliman <nsuliman88@gmail.com>
2020-03-02 01:42:27 +0200

    trying with the lab

--- src/modules/to-do-list.js
+++ src/modules/to-do-list.js
@@ -1,7 +1,35 @@
 import React from 'react';
 // hook useState ( build-in )
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 
 function HooksToDoList(){
+
+    const [complete,setComplete] = useState(false);
+    const [difficulty,setDifficulty] = useState(0);
+
+    const updateCounters = () => {
+        let newStatus = !complete;
+        if(newStatus === false) {
+            newStatus = 'incomplete';
+        } else {
+            newStatus = 'complete';
+        }
+        setComplete(newStatus);
+
+        setDifficulty()
+    }
+
+    return (
+        <section>
+            <form onSubmit = {}>
+                <input onChange={}></input>
+            </form>
+            {
+                items.map(item =>{
+                    <p key={item}>{item}</p>
+                })
+            }
+        </section>
+    )
 
 } // end of HooksToDoList function
