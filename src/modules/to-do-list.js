c4ce5a5d7edc7e8d110aadeb46319e6fc1b4ab03
Nsuliman <nsuliman88@gmail.com>
2020-03-07 22:00:59 +0200

    change the status , add the item done

--- src/modules/to-do-list.js
+++ src/modules/to-do-list.js
@@ -19,10 +19,11 @@ function HooksToDoList() {
     // }
 
 
-    // set items 
+    // set status if complete or incomplete 
+    const [status, setStatus] = useState('complete');
+
     const [items, setItems] = useState([]);
     const [assignedTo, setAssignedTo] = useState('');
-    const [status, setStatus] = useState(false);
 
     const _changeAssigned = (e) => {
         setAssignedTo(e.target.value);
@@ -34,39 +35,41 @@ function HooksToDoList() {
         assignedTo && setItems([...items, assignedTo]);
     } // end of _addItem
 
-    const _updateCounters = () => {
-        let newStatus = !status;
-        setStatus(newStatus);
-    }
+    // const _updateCounters = () => {
+    //     let newStatus = !status;
+    //     setStatus(newStatus);
+    // }
     /****************************************************** Use Effect ***********************************************/
     // for each render 
-    useEffect(() => {
-        console.log('Welcome @ To Do List  ');
-    })
+    // useEffect(() => {
+    //     console.log('Welcome @ To Do List  ');
+    // })
 
-    // for assignedTo 
-    useEffect(() => {
-        console.log('assignedTo : ', assignedTo);
-    }, [assignedTo]);
+    // // for assignedTo 
+    // useEffect(() => {
+    //     console.log('assignedTo : ', assignedTo);
+    // }, [assignedTo]);
 
     // for status 
     useEffect(() => {
-        if (status) { document.title = 'Complete' }
+        if (items.length>=1) { document.title = 'Complete' }
         else { document.title = 'Incomplete' }
-    }, [status]);
+    }, [items]);
 
 
     /****************************************************** Rendering ***********************************************/
     return (
         <section>
             <form onSubmit={_addItem}>
-                <input onChange={_changeAssigned}></input>
+                <p> Enter Item To Do List : 
+                <input onChange={_changeAssigned}></input> 
+                </p>
             </form>
-            {/* {
-                items.map(item => {
+            {
+                items.map(item => 
                     <p key={item}>{item}</p>
-                })
-            } */}
+                )
+            }
         </section>
     )
 
