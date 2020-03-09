f85e16ea387056ecb12746df330d3a60ff53b3e1
Nsuliman <runner@fv-az50.jolctyf0bquutotllhlthlevad.cx.internal.cloudapp.net>
2020-03-09 04:11:36 +0000

    Updates

---
+++ precache-manifest.422e29076fc672f1a62a010fd6fcf1d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b3d5a00aeb38eba02b1e752becf62c",
+    "url": "/Lab-31-To-Do-Application/index.html"
+  },
+  {
+    "revision": "f03a30f139b06e69b913",
+    "url": "/Lab-31-To-Do-Application/static/css/main.b61477b4.chunk.css"
+  },
+  {
+    "revision": "1a5fad749687214a8e22",
+    "url": "/Lab-31-To-Do-Application/static/js/2.52fedc9d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Lab-31-To-Do-Application/static/js/2.52fedc9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03a30f139b06e69b913",
+    "url": "/Lab-31-To-Do-Application/static/js/main.b4f0d92e.chunk.js"
+  },
+  {
+    "revision": "d35a8e4a253094e7a930",
+    "url": "/Lab-31-To-Do-Application/static/js/runtime-main.4c581e88.js"
+  }
+]);
